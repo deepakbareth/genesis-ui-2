79d0bf9a3ab8f6c2b784499bc9d2c11f8f0a17ef
deepak bareth <deepakbareth7735@gmail.com>
2026-07-09 00:48:05 +0530

    page start from top

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,17 +1,4 @@
-import Banner from "../Components/Home/Banner";
-import ExecutiveCarousel from "../Components/Home/ExecutiveCarousel";
-import FeatureSection from "../Components/Home/FeatureSection";
-import ImageGallery from "../Components/Home/ImageGallery";
-import ImpactSection from "../Components/Home/ImpactSection/ImpactSection";
-import LeadershipImpact from "../Components/Home/LeadershipImpact";
-import MidBanner from "../Components/Home/MidBanner";
-import ProjectInfoBar from "../Components/Home/ProjectInfoBar";
-import TimelineSection from "../Components/Home/TimelineSection";
-import WorkforceSection from "../Components/Home/WorkforceSection";
-import Footer from "../Components/Layout/Footer";
 
-import Navbar from "../Components/Layout/Navbar";
-import homeBannerVideo from "../assets/banner2.mp4"
 import ServicesBanner from "../Components/ServicesPage/ServicesBanner";
 // import ServiceSplitSection from "../Components/ServicesPage/ServiceSplitSection";
 // import { hydrogenData } from "../Data/ServicesData";
